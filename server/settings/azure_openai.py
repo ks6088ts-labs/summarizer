a1319327fed6f95cc18f2a552f02b8dbdc243786
ks6088ts <ks6088ts@gmail.com>
2024-01-10 23:12:14 +0900

    add settings module

---
+++ server/settings/azure_openai.py
@@ -0,0 +1,17 @@
+from pydantic import (
+    Field,
+)
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AzureOpenAiSettings(BaseSettings):
+    api_version: str = Field("2023-07-01-preview", alias="api_version")
+    azure_endpoint: str = Field(
+        "https://YOUR_AOAI_NAME.openai.azure.com/", alias="azure_endpoint"
+    )
+    azure_deployment: str = Field("gpt-35-turbo", alias="azure_deployment")
+    api_key: str = Field("YOUR_API_KEY", alias="api_key")
+
+    model_config = SettingsConfigDict(
+        env_file="azure_openai.env", env_file_encoding="utf-8"
+    )
